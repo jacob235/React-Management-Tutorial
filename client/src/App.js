9402d893df06d78604e3dc191bc809982e13356d
Wonjae <DooWonJae@Wonjaes-MacBook-Pro.local>
2019-12-05 18:24:52 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
@@ -17,19 +18,22 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress:{
+    margin: theme.spacing.unit * 2
   }
 });
 
 
 class App extends Component {
   state = {
-    customers: ""
+    customers: '',
+    completed: 0
   }
 
   componentDidMount(){
-    this.callApi()
-      .then(res => this.setState({customers: res}))
-      .catch(err => console.log(err));
+    this.timer = setInterval(this.progress, 20);
+    this.callApi().then(res => this.setState({customers: res})).catch(err => console.log(err));
   }
 
   callApi = async () => {
@@ -38,6 +42,10 @@ class App extends Component {
     return body;
   }
 
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1})
+  }
 
   render(){
     const { classes } = this.props;
@@ -55,8 +63,14 @@ class App extends Component {
             </TableRow>
           </TableHead>
         <TableBody>
-        {this.state.customers ? this.state.customers.map(c=>{ return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
-        }) : ""}
+        {this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+        }) : 
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+        }
         </TableBody>
         </Table>
       </Paper>
